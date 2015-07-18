e9e41a839eb216b0357939889e0a13da993faa6a
serp777 <arusse02@gmail.com>
2015-07-17 19:23:16 -0700

    build new bucket

--- build-bucket.sh
+++ build-bucket.sh
@@ -1,13 +1,18 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
 sudo mkdir "$1"
 cd "$1"
 sudo mkdir img
-git init
-git add .
-git commit -m 'build new bucket'
-git remote add origin https://github.com/serp777/Tixzoo.git
-git push -u origin master
-git checkout master
-wget https://drive.google.com/file/d/0B5ld2CqQANLaMG4tYmhvMTJGTUk/view?usp=sharing
-unzip images.zip img/
+sudo git clone https://github.com/serp777/Tixzoo.git
+sudo git init
+sudo git add .
+sudo git commit -m 'build new bucket'
+sudo git remote add origin https://github.com/serp777/Tixzoo.git
+sudo git push -u origin master
+sudo git checkout master
+cd Tixzoo
+sudo wget https://drive.google.com/file/d/0B5ld2CqQANLaMG4tYmhvMTJGTUk/view?usp=sharing
+sudo unzip images.zip img/
+cd ..
 cd ..
 sudo chmod -R 777 "$1"
