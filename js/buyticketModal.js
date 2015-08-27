c25e7a06efb2241bea454327350e32beb4b92a95
dimarinez <dimarinez@yahoo.com>
2015-08-26 21:33:32 -0700

    minor adjustments

--- js/buyticketModal.js
+++ js/buyticketModal.js
@@ -2,5 +2,20 @@
  * CreateTicketModalController
  */
 App.BuyticketModalController = Ember.ObjectController.extend({
+  needs: ['application'],
   maxFrat: 'img/max-frat.png',
+  creditCard: 'view-ticket-info',
+  actions: {
+  	creditCard: function() {
+      console.log('test');
+      if(this.get('creditCard') == 'view-ticket-info'){
+        this.set('creditCard','view-ticket-info:target');
+      } else {
+        this.set('creditCard','view-ticket-info');
+      }
+    }, 
+  temp: function() {
+
+  }
+  }
 });
